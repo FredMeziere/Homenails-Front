10cc9bbbba5a4ec1191ba856486f88d6ee0cc346
Claire Vanlerberghe <vanlerberghe24@gmail.com>
2023-03-21 13:13:35 +0100

    Page error+css

--- homenails/src/Components/pages/Error/Error.js
+++ homenails/src/Components/pages/Error/Error.js
@@ -1,8 +1,12 @@
-
+import { NavLink } from 'react-router-dom'
 
 function Error () {
   return(
-    <p>Error</p>
+    <section className="error">
+      <p className="errorNumber">404</p>
+      <p className="errorTitle">Oups, la page que vous demandez n'existe pas...</p>
+      <NavLink className="errorLink" to="/">Retourner sur la page d'accueil</NavLink>
+    </section>
   )
   }
   
