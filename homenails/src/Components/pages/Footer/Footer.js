7b48dd12c21f307dca3bf10264ffe2a5058a39a6
Claire Vanlerberghe <vanlerberghe24@gmail.com>
2023-03-22 10:20:16 +0100

    Début du contenu footer

--- homenails/src/Components/pages/Footer/Footer.js
+++ homenails/src/Components/pages/Footer/Footer.js
@@ -1,8 +1,16 @@
-
+import logoFooter from "../../../logoSansFond/déclinaisonFooter.png"
+import Button from "../../Button/Button";
 
 function Footer () {
     return(
       <section className="footer">
+        <img className="footerIMG" src={logoFooter} alt="Déclinaison du logo pour le footer" />
+        <form className="footerNeswletter">
+          <p>Abonnez-vous à notre newsletter :</p>
+          <input type="text" placeholder="Votre email..." />
+          <Button titre="S'abonner" link="/" />  
+          
+        </form>
         
       </section>
     )
