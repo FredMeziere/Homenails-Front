7b5d7b3a79c8a4c4c2dd78bc2fd3135255dd5c24
Andy-Vasseur <andyvasseurpro@outlook.com>
2023-03-22 11:54:11 +0100

    feat: contact page created and styled

--- homenails/src/Components/pages/Contact/Contact.js
+++ homenails/src/Components/pages/Contact/Contact.js
@@ -1,9 +1,34 @@
+function Contact() {
+  return (
+    <div className="Contact">
+      <div className="contact-container">
+        <h1>Formulaire de contact</h1>
+        <form method="post">
+          <input type="text" placeholder="Votre prénom..." />
+          <input type="text" placeholder="Votre nom..." />
+          <input type="tel" placeholder="Votre téléphone..." />
+          <input type="email" placeholder="Votre email..." />
+          <textarea placeholder="Votre message..." />
+          <button>Envoyer</button>
+        </form>
+      </div>
 
+      <div className="infos-container">
+        <div className="logo" />
+        <div>
+          <h2>Claire Vanlerberghe</h2>
+          <div>
+          <p>79 rue du feretra</p>
+          <p>31400 Toulouse</p>
+          </div>
+          <div>
+          <p>vanlerberghe24@gmail.com</p>
+          <p>06.07.42.78.05</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-function Contact () {
-    return(
-      <p>Contact</p>
-    )
-  }
-  
-  export default Contact;
+export default Contact;
