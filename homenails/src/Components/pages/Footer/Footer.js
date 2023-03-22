fc5c185188ca4e90ff0c9cc8b1b1a667fd38246f
Claire Vanlerberghe <vanlerberghe24@gmail.com>
2023-03-22 17:24:44 +0100

    Mise en page du footer et arrangement css header et page error

--- homenails/src/Components/pages/Footer/Footer.js
+++ homenails/src/Components/pages/Footer/Footer.js
@@ -1,5 +1,6 @@
 import logoFooter from "../../../logoSansFond/déclinaisonFooter.png"
 import Button from "../../Button/Button";
+import Menu from "../../Menu/Menu"
 
 function Footer () {
     return(
@@ -9,8 +10,13 @@ function Footer () {
           <p>Abonnez-vous à notre newsletter :</p>
           <input type="text" placeholder="Votre email..." />
           <Button titre="S'abonner" link="/" />  
-          
         </form>
+        <div className="footerMenu">
+          <Menu titre="CGV" link="/cgv" />
+          <Menu titre="Mentions légales" link="/legacy" />
+          <Menu titre="A propos" link="/about" />
+          <Menu titre="Politique de confidentialité" link="/privacy" />
+        </div>
         
       </section>
     )
