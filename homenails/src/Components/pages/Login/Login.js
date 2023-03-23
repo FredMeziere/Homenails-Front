c01b1ca6f09fec813f59c5107431def985c2dff2
Andy-Vasseur <beaugosswwe@live.fr>
2023-03-23 12:47:32 +0100

    feat: login page created and styled

--- homenails/src/Components/pages/Login/Login.js
+++ homenails/src/Components/pages/Login/Login.js
@@ -1,9 +1,18 @@
-
-
-function Login () {
-  return(
-    <p>Login</p>
+function Login() {
+  return (
+    <div className="Login">
+      <h1>Login</h1>
+      <div className="login-container">
+        <p>Vous n'avez pas encore de compte ? Cliquez <a href="/signup">ici</a></p>
+        <p>Sinon, connectez-vous ici :</p>
+        <form method="post">
+          <input type="text" placeholder="Votre email..." />
+          <input type="password" placeholder="Votre mot de passe..." />
+          <button>Se connecter</button>
+        </form>
+      </div>
+    </div>
   )
-  }
-  
-  export default Login;
+}
+
+export default Login;
